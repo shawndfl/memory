6f89574d27806d7e92a857b6ac4e6b7ebdb89597
ShawnD <shawn@gmail.com>
2018-08-15 02:04:21 +0000

    Add input test

--- test/src/readTest.c
+++ test/src/readTest.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include "input.h"
 
-#define FAIL 1
-#define SUCCED 0;
+#define FAIL   1
+#define SUCCED 0
 
 int startTestApp();
 
 int main() {
- 
+   
   startTestApp();
-  return FAIL;
+  printf("Test Passed\n");
+  return SUCCED;
 }
 
 int startTestApp() {
- return 0;
+  printf("Starting app...\n");
+  char* line = inputLine ();
+  printf("Line is %s\n", line);
+  
+  return 0;
 }
